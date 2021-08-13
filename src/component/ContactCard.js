736ead48a8475adb0b216de0702ffe85cd24ea0a
zzarrafa <57454465+zzarrafa@users.noreply.github.com>
2021-08-13 20:19:01 +0100

    Update ContactCard.js

--- src/component/ContactCard.js
+++ src/component/ContactCard.js
@@ -2,12 +2,12 @@ import React from "react";
 
 
 const ContactCard = (props) => {
-    // const { id, name, email } = props.contact;
+    const { id, name, email } = props.contact;
     return (
         <div className="item">
                 <div className="content">
-                    <div className="header">{props.name}</div>
-                    <div>props.contact.email</div> 
+                    <div className="header">{name}</div>
+                    <div>email</div> 
                 </div>
                 <i className="trash alternate outline icon"
                     style ={{color : "red ", marginTop : "7px"}}>
